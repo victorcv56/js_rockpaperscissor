c974f1d6a767c969ae36d091eaf8cff57143245e
victorcv56 <victorcv56@gmail.com>
2024-08-19 18:48:01 -0700

    add humanChoice method to prompt user for answer for game.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,17 +1,36 @@
 
 function getComputerChoice() {
-    num = Math.round((Math.random() * 10));
-    console.log(num);
-    if (num < 3 + 1) console.log('Rock');
-    if (num > 3  && num < 6 + 1) console.log('Paper');
-    if (num > 6 ) console.log('Scissor');
-    scount = 0;
-    pcount = 0;
-    rcount = 0;
+    const getRandomNumner = (min, max) => {
+        return Math.floor(Math.random() * (max - min) + min)
+        }
 
+    let scount = 0;
+    let pcount = 0;
+    let rcount = 0;
+
+    let num = getRandomNumner(1, 10);
+    let choice = '';
+
+    console.log(num);
+    if (num < 3 + 1) choice + 'Rock';
+    if (num > 3  && num < 6 + 1) choice + 'Paper';
+    if (num > 6 ) choice + 'Scissors';
 }
 
-getComputerChoice();
+function getHumanChoice() {
+    let choice = prompt('Rock, Paper or Scissors?: ');
+    return choice.toLowerCase();
+}
+
+function decideWinner(hChoice, cChoice) {
+    let score = 0;
+    
+}
+let compChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
+
+
+
 
 
 /*
@@ -24,3 +43,9 @@ Another thing to consider is math.random() gives us a number
 between 0-1 which we can convert to whole numbers if i multiply
 by 10, but i need to stop at 9..
 */
+
+// Random number within a range formula: 
+
+// const getRandomNumner = (min, max) {
+//     return Math.random() * (max - min) + min
+// }
